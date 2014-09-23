f8c61140e0bace0c765ae0daf31a1768a2bb4cd0
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-09-23 18:21:58 +0300

    MARKO PULL

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var connect = require('connect')
   ,http = require('http');
-var connectRouter = require('connect-router');
+//var connectRouter = require('connect-router');
 
 var app = connect();
 
-connectRouter(__dirname + '/routes');
+app.use('/', function(req, res){
+  res.end('Welcome to Connect!\n');
+});
+//connectRouter(__dirname + '/routes');
 
 
 module.exports = http.createServer(app).listen(3000);
