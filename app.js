bfa37b5ac110d6f1466ea80abcafbefd9116174f
Marko <marko.hamppula@metropolia.fi>
2014-09-23 18:16:28 +0300

    Shit happens

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 var connect = require('connect')
   ,http = require('http');
-
-var path = require('path');
-//var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var hjs = require('hjs');
-var routes = require('./routes/index');
 var connectRouter = require('connect-router');
 
 var app = connect();
@@ -15,4 +7,4 @@ var app = connect();
 connectRouter(__dirname + '/routes');
 
 
-module.exports = app;
+module.exports = http.createServer(app).listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,56 @@
-var express = require('express');
-var router = express.Router();
 var validator = require('validator');
-var mailer = require('../lib/mailer'),
+var mailer = require('../lib/mailer');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+exports.plugin = function(router)
+{
+    /* GET home page. */
+    router.on('/', function(req, res) {
+      res.send("Plah");
+    });
 
-router.on('validate/token', function(req, res, next) {
+    router.on('validate/token', function(req, res, next) {
 
-  var resObj = {
-    message: '',
-    success: true,
-    error: []
-  }
+      var resObj = {
+        message: '',
+        success: true,
+        error: []
+      }
 
-  if(!req.body.name){
-      resObj.error.push("Missing name");
-      resObj.success = false;
-  }
-  if(!req.body.email || !validator.isEmail(req.body.email)){
-    resObj.error.push("Missing or invalid email");
-    resObj.success = false;
+      if(!req.body.name){
+          resObj.error.push("Missing name");
+          resObj.success = false;
+      }
+      if(!req.body.email || !validator.isEmail(req.body.email)){
+        resObj.error.push("Missing or invalid email");
+        resObj.success = false;
 
-  }
+      }
 
-  if(!req.body.feedback){
-    resObj.error.push("Missing feedback");
-    resObj.success = false;
-  }
+      if(!req.body.feedback){
+        resObj.error.push("Missing feedback");
+        resObj.success = false;
+      }
 
-  if(resObj.success){
-    var mailOptions = {
-      to: resObj.email,
-      from: 'Lähettäjä <marko@onparas.com>',
-      subject: 	'Feedback from test site',
-      text: res.Obj.message
-    };
-    mailer.send(mailOptions, function(err) {
-      if (err) console.log(err);
+      if(resObj.success){
+        var mailOptions = {
+          to: resObj.email,
+          from: 'Lähettäjä <marko@onparas.com>',
+          subject: 	'Feedback from test site',
+          text: res.Obj.message
+        };
+        mailer.send(mailOptions, function(err) {
+          if (err) console.log(err);
 
-      return res.redirect('/');
-  	});
-    resObj.message = "Feedback sent successfully";
-    res.status(200).send(resObj);
-    next();
-  }
-  else{
-    res.status(403).send(resObj);
-  }
+          return res.redirect('/');
+        });
+        resObj.message = "Feedback sent successfully";
+        res.status(200).send(resObj);
+        next();
+      }
+      else{
+        res.status(403).send(resObj);
+      }
 
-});
+    });
 
-module.exports = router;
+}
